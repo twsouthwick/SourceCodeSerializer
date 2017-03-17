a4d4ab2d62f19cb998f2da23d383f7b1b3a3f929
Taylor Southwick <tasou@microsoft.com>
2017-03-17 09:40:20 -0700

    Fix versioning

--- Update-Version.ps1
+++ Update-Version.ps1
@@ -2,12 +2,7 @@ param([Parameter(Mandatory = $true)] $version)
 
 Write-Host "Updating version to $version"
 
-$project = "src\SourceCodeSerializer\project.json"
-$content = Get-Content $project
-$newcontent = $content.Replace("`"version`": `"1.0.0-*`"","`"version`": `"$version`"")
-Set-Content $project $newcontent
-
-$project = "test\SourceCodeSerializer.Test\project.json"
-$content = Get-Content $project
-$newcontent = $content.Replace("`"SourceCodeSerializer`": `"1.0.0-*`"","`"SourceCodeSerializer`": `"$version`"")
-Set-Content $project $newcontent
+$props = "$PSScriptRoot\src\SourceCodeSerializer\SourceCodeSerializer.csproj"
+[xml]$dirprops = Get-Content $props
+$dirprops.Project.PropertyGroup.VersionPrefix = $version
+$dirprops.Save($props)
